ab6b7a479f1e2008bb806a7330328ba949bba631
MCLEANS <jackagwaya@gmail.com>
2020-06-09 12:26:49 +0300

    Obtain GPS values

--- atmega328p_firmware/src/main.cpp
+++ atmega328p_firmware/src/main.cpp
@@ -56,6 +56,59 @@ void fetchSensorDHT(float &t, float &h){
 		}
 }
 
+/*****************************************************************
+ * read GPS sensor values                                        *
+ *****************************************************************/
+
+void fetchSensorGPS()
+{
+  if (gps.location.isUpdated()) {
+		if (gps.location.isValid()) {
+			last_value_GPS_lat = gps.location.lat();
+			last_value_GPS_lon = gps.location.lng();
+		} else {
+			last_value_GPS_lat = -200;
+			last_value_GPS_lon = -200;
+			Serial.println(F("Lat/Lng INVALID"));
+		}
+		if (gps.altitude.isValid()) {
+			last_value_GPS_alt = gps.altitude.meters();
+			String gps_alt(last_value_GPS_lat);
+		} else {
+			last_value_GPS_alt = -1000;
+			Serial.println(F("Altitude INVALID"));
+		}
+		if (gps.date.isValid()) {
+			char gps_date[16];
+			snprintf_P(gps_date, sizeof(gps_date), PSTR("%02d/%02d/%04d"),
+					gps.date.month(), gps.date.day(), gps.date.year());
+			last_value_GPS_date = gps_date;
+		} else {
+			Serial.println(F("Date INVALID"));
+		}
+		if (gps.time.isValid()) {
+			char gps_time[20];
+			snprintf_P(gps_time, sizeof(gps_time), PSTR("%02d:%02d:%02d.%02d"),
+				gps.time.hour(), gps.time.minute(), gps.time.second(), gps.time.centisecond());
+			last_value_GPS_time = gps_time;
+		} else {
+			Serial.println(F("Time: INVALID"));
+		}
+	}
+
+	Serial.print("LOCATION ");
+	Serial.print(last_value_GPS_lat);
+	Serial.print(" , ");
+	Serial.print(last_value_GPS_lon);
+	Serial.print(" , ");
+	Serial.println(last_value_GPS_alt);
+	Serial.print("Date ");
+	Serial.print(last_value_GPS_date);
+	Serial.print(" TIME ");
+	Serial.println(last_value_GPS_time);
+
+}
+
 
 void setup() {
 
