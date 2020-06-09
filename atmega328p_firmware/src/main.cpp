461b6edca445f2f37f10582f1052d98f8d32b6fe
MCLEANS <jackagwaya@gmail.com>
2020-06-09 11:46:15 +0300

    Added DHT functionality

--- atmega328p_firmware/src/main.cpp
+++ atmega328p_firmware/src/main.cpp
@@ -1,9 +1,53 @@
 #include <Arduino.h>
+#include "DHT.h"
+
+#define ONEWIRE_PIN 2
+#define DHT_TYPE DHT22
+
+/*****************************************************************
+ * DHT declaration                                               *
+ *****************************************************************/
+DHT dht(ONEWIRE_PIN, DHT_TYPE);
+
+/******************************************************************
+ *  DHT VARIABLES
+ * ***************************************************************/
+float dht_humidity = 0.0;
+float dht_temperature = 0.0;
+
+/*****************************************************
+ * READ DHT SENSOR VALUES
+ ********************************************************/
+void fetchSensorDHT(float &t, float &h){
+
+  	t = dht.readTemperature();
+	  h = dht.readHumidity();
+
+		if (isnan(t) || isnan(h)) {
+			delay(100);
+			t = dht.readTemperature(false);
+			h = dht.readHumidity();
+		}
+		if (isnan(t) || isnan(h)) {
+			Serial.println(F("DHT11/DHT22 read failed"));
+		}
+}
+
 
 void setup() {
-  // put your setup code here, to run once:
+
+  Serial.begin(9600);
+  dht.begin();
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+  fetchSensorDHT(dht_temperature,dht_humidity);
+
+  Serial.print("TEMPERATURE : ");
+  Serial.print(dht_temperature);
+  Serial.print(" HUMIDITY : ");
+  Serial.println(dht_humidity);
+
 }
