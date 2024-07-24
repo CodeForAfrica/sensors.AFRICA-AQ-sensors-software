58ce1512a3f9e39e8206990580945868e48210e8
gideon maina <gmainapro@yahoo.com>
2024-07-24 16:24:12 +0300

    Prevent GSM soft reset in successive GSM init calls

--- airrohr-firmware/GSM_handler.h
+++ airrohr-firmware/GSM_handler.h
@@ -29,7 +29,7 @@ void enableGPRS();
 void flushSerial();
 
 int GPRS_INIT_FAIL_COUNT = 0;
-
+bool INITIAL_GSM_BOOT = false;
 // Set a decent delay before this to warm up the GSM module
 bool GSM_init(SoftwareSerial *gsm_serial)
 { // Pass a ptr to SoftwareSerial GSM instance
@@ -39,6 +39,12 @@ bool GSM_init(SoftwareSerial *gsm_serial)
     // restart GSM if the GSM is powered separately to remove pre-existing configurations
     String error_msg = "";
     // Check if there is serial communication with a GSM module
+    if (SEPARATE_GSM_PWR && !INITIAL_GSM_BOOT)
+    {
+        GSM_soft_reset();
+        INITIAL_GSM_BOOT = true;
+    }
+
     if (!fona.begin(*gsm_serial))
     {
         error_msg = "Could not find GSM module";
@@ -46,10 +52,6 @@ bool GSM_init(SoftwareSerial *gsm_serial)
         Serial.println(error_msg);
         GSM_CONNECTED = false;
         return false;
-    }
-    if (SEPARATE_GSM_PWR)
-    {
-        GSM_soft_reset();
     }
 
     Serial.print("GSM module found!");
