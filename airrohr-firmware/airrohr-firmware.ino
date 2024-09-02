c68e7a5d4b8160af33dea8b6f0879d565227db76
gideon maina <gmainapro@yahoo.com>
2024-09-02 11:19:14 +0300

    Refactor: Dallas DS18B20

--- airrohr-firmware/airrohr-firmware.ino
+++ airrohr-firmware/airrohr-firmware.ino
@@ -92,7 +92,6 @@ String SOFTWARE_VERSION(SOFTWARE_VERSION_STR);
 #include <DNSServer.h>
 #include <SPI.h>
 #include <StreamString.h>
-#include <DallasTemperature.h>
 #include <TinyGPS++.h>
 #include "./sps30_i2c.h"
 #include <Adafruit_FONA.h>
@@ -176,12 +175,6 @@ SoftwareSerial *serialGPS;
 float last_value_dnms_laeq = -1.0;
 float last_value_dnms_la_min = -1.0;
 float last_value_dnms_la_max = -1.0;
-
-/*****************************************************************
- * DS18B20 declaration                                            *
- *****************************************************************/
-OneWire oneWire(ONEWIRE_PIN);
-DallasTemperature ds18b20(&oneWire);
 
 /*****************************************************************
  * GPS declaration                                               *
@@ -372,6 +365,7 @@ const char JSON_SENSOR_DATA_VALUES[] PROGMEM = "sensordatavalues";
 #include "./sensors/Temperature_Humidity/DHT/DHT_func.h"
 #include "./sensors/Temperature_Humidity/HTU21DF/HTU21DF.h"
 #include "./sensors/Temperature_Humidity/SHT3X/SHT31.h"
+#include "./sensors/Temperature_Humidity/Dallas/ds18b20.h"
 #include "./sensors/Pressure/BMP085.h"
 #include "./sensors/Pressure/BMX280.h"
 
@@ -1263,41 +1257,6 @@ static void send_csv(const String &data)
 	{
 		debug_outln_error(FPSTR(DBG_TXT_DATA_READ_FAILED));
 	}
-}
-
-/*****************************************************************
- * read DS18B20 sensor values                                    *
- *****************************************************************/
-static void fetchSensorDS18B20(String &s)
-{
-	float t;
-	debug_outln_verbose(FPSTR(DBG_TXT_START_READING), FPSTR(SENSORS_DS18B20));
-
-	// it's very unlikely (-127: impossible) to get these temperatures in reality. Most times this means that the sensor is currently faulty
-	// try 5 times to read the sensor, otherwise fail
-	const int MAX_ATTEMPTS = 5;
-	int count = 0;
-	do
-	{
-		ds18b20.requestTemperatures();
-		// for now, we want to read only the first sensor
-		t = ds18b20.getTempCByIndex(0);
-		++count;
-		debug_outln_info(F("DS18B20 trying...."));
-	} while (count < MAX_ATTEMPTS && (isnan(t) || t >= 85.0f || t <= (-127.0f)));
-
-	if (count == MAX_ATTEMPTS)
-	{
-		last_value_DS18B20_T = -128.0;
-		debug_outln_error(F("DS18B20 read failed"));
-	}
-	else
-	{
-		last_value_DS18B20_T = t;
-		add_Value2Json(s, F("DS18B20_temperature"), FPSTR(DBG_TXT_TEMPERATURE), last_value_DS18B20_T);
-	}
-	debug_outln_info(FPSTR(DBG_TXT_SEP));
-	debug_outln_verbose(FPSTR(DBG_TXT_END_READING), FPSTR(SENSORS_DS18B20));
 }
 
 /*****************************************************************
