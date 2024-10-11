2f47279be6e390118dbebb76ba5e20b0591aaf24
gideon maina <gmainapro@yahoo.com>
2024-10-11 11:09:50 +0300

    Fix: OTA update filename and upload progress status

--- airrohr-firmware/webserver/webserver.h
+++ airrohr-firmware/webserver/webserver.h
@@ -1119,7 +1119,7 @@ static void webserver_render_ota_upload_page()
     page_content += "r.onreadystatechange=function(){if(4===r.readyState&&200===r.status){console.log(r.responseText);let e=r.getResponseHeader('Content-Type');";
     page_content += " e.includes('text/html')?document.body.innerHTML=r.responseText:(msgLogger(r.responseText),setTimeout(()=>{let e=' <p>File Saved Successfully!</p><p>Attempting firmware update...</p>';document.body.innerHTML=e},2e3))}},";
     page_content += "r.upload.onloadstart=function(e){console.log('upload started'),console.log(`Event bytes: ${e.total} `),progressWrapper.style.display='flex'},r.upload.onprogress=function(e){console.log(`Loaded ${e.loaded} bytes of ${e.total}`),";
-    page_content += "progressBar.max=e.total,progressBar.value=e.loaded,progressText.innerText=100*Math.ceil(e.loaded/e.total)+'%'},r.upload.ontimeout=function(){msgLogger('Upload timeout','error')},r.upload.onerror=function(){msgLogger('Error uploading file','error')},";
+    page_content += "progressBar.max=e.total,progressBar.value=e.loaded,progressText.innerText=Math.ceil(e.loaded/e.total*100)+'%'},r.upload.ontimeout=function(){msgLogger('Upload timeout','error')},r.upload.onerror=function(){msgLogger('Error uploading file','error')},";
     page_content += "r.timeout=3e4,r.open(t,o),r.send(s)}const form=document.querySelector('#ota_form'),inputFile=document.querySelector('#firmware'),progressWrapper=document.querySelector('#progress_wrapper'),progressBar=document.querySelector('#ota_progress'),";
     page_content += "progressText=document.querySelector('#progress_text'),log=document.createElement('p');form.addEventListener('submit',handleSubmit);";
     page_content += "</script>";
@@ -1152,16 +1152,16 @@ void uploadFiles()
                 return;
             }
         }
-        if (!fname.startsWith("/"))
-            fname = "/" + new_firmware_filename; // assign any bin file to a new file name
-        Serial.print("Upload File Name: ");
-        Serial.println(fname);
+
+        fname = new_firmware_filename; // assign any bin file to a new file name
+
+        // Serial.print("Upload File Name: ");
+        // Serial.println(fname);
         uploadFile = SPIFFS.open(fname, "w"); // Open the file for writing in SPIFFS (create if it doesn't exist)
         if (uploadFile)
         {
             Serial.println("File opened");
         }
-        // fname = String();
         Serial.print("fname: ");
         Serial.println(fname);
     }
@@ -1183,8 +1183,8 @@ void uploadFiles()
             Serial.println(upload.totalSize);
             String msg = "201: Successfully uploaded file ";
             msg += fname;
-            // server.send(200, "text/plain", msg);
-            server.send(200, FPSTR(TXT_CONTENT_TYPE_TEXT_HTML), FPSTR("<html><body><p>File(s) uploaded successfully</p><p>Attempting firmware update....</body></html>)"));
+            server.send(200, "text/plain", msg);
+            // server.send(200, FPSTR(TXT_CONTENT_TYPE_TEXT_HTML), FPSTR("<html><body><p>File(s) uploaded successfully</p><p>Attempting firmware update....</body></html>)"));
             Serial.println(msg);
             firmware_bin_saved = true;
             delay(2000);
