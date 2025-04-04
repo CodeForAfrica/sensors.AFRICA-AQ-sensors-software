92c6e84b657bb1976e10ba510ea2b8fe9b96c6f9
gideon maina <gmainapro@yahoo.com>
2025-04-04 18:26:56 +0300

    Fix: include recursive activate GPRS call

--- airrohr-firmware/GSM_handler.h
+++ airrohr-firmware/GSM_handler.h
@@ -693,19 +693,22 @@ bool activateGPRS()
         case NetMode::AUTO:
             current_network = NetMode::_2G;
             register_to_network();
+            activateGPRS();
             break;
         case NetMode::_2G:
             current_network = NetMode::_4G;
             register_to_network();
+            activateGPRS();
             break;
         case NetMode::_4G:
             current_network = NetMode::AUTO;
             register_to_network();
+            activateGPRS();
             break;
         }
     }
 
-    Serial.println("Failed to enable GPRS");
+    // Serial.println("Failed to enable GPRS"); //? will be printed bt previous recursive call
     return false;
 }
 
